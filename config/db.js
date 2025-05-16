eeb62ad7e6007c49d053ce0288e46ff280a7a6b6
DANAVPS <ddannavvalentina@gmail.com>
2025-05-15 21:26:48 -0500

    fix: update database configuration in db.js and remove obsolete backup.sql file

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 import Sequelize from 'sequelize';
 
 const db = new Sequelize(
-    process.env.DB_NAME || 'agenciadeviajes',
-    process.env.DB_USER || 'root',
-    process.env.DB_PASSWORD || '',
+    process.env.DB_NAME || 'agenciadeviajes_commonrain',
+    process.env.DB_USER || 'agenciadeviajes_commonrain',
+    process.env.DB_PASSWORD || '921ea7f39737ad4f2b3e91edc73b9b0d5a564aef',
     {
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || '3306',
+        host: process.env.DB_HOST || '10yus.h.filess.io',
+        port: process.env.DB_PORT || '3307',
         dialect: 'mysql',
         define: {
             timestamps: false
